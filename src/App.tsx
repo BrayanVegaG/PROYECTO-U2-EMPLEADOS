77f56adfdd8c2bbaafdf7103cbee0bd78dbf1d7d
Brayan Vega <93897118+BrayanVegaG@users.noreply.github.com>
2025-01-23 15:07:47 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Empleados from './Componentes/Empleados';
 import Habilidades from './Componentes/Habilidades';
 import RelacionHabilidadesEmpleados from './Componentes/RelacionEmple-Hab';
 import Inicio from './Componentes/Inicio';
 import AcercaDe from './Componentes/AcercaDe';
-import Navbar from './Componentes/NavBar'; // Importamos el nuevo Navbar
 import './App.css';
 
 interface Empleado {
-  id: number;
-  nombre: string;
-  puesto: string;
-  idHabilidad: number;
+    id: number;
+    nombre: string;
+    puesto: string;
+    idHabilidad: number;
 }
 
 interface Habilidad {
-  id: number;
-  NombreHabilidad: string;
-  idEmpleado: number;
+    id: number;
+    NombreHabilidad: string;
+    idEmpleado: number;
 }
 
+interface Relacion {
+    id_empleado: number;
+    id_habilidad: number;
+  }
+
 const App: React.FC = () => {
-    const [empleados, setEmpleados] = useState<Empleado[]>([]);
-    const [habilidades, setHabilidades] = useState<Habilidad[]>([]);
+        const [empleados, setEmpleados] = useState<Empleado[]>([]);
+        const [habilidades, setHabilidades] = useState<Habilidad[]>([]);
+        const [relaciones, setRelaciones] = useState<Relacion[]>([]);
 
-    useEffect(() => {
-        const storedEmpleados = localStorage.getItem('empleados');
-        const storedHabilidades = localStorage.getItem('habilidades');
+        useEffect(() => {
+                const storedEmpleados = localStorage.getItem('empleados');
+                const storedHabilidades = localStorage.getItem('habilidades');
+                const storedRelaciones = localStorage.getItem('relaciones');
 
-        if (storedEmpleados) {
-            setEmpleados(JSON.parse(storedEmpleados));
-        }
+                if (storedEmpleados) {
+                        setEmpleados(JSON.parse(storedEmpleados));
+                }
 
-        if (storedHabilidades) {
-            setHabilidades(JSON.parse(storedHabilidades));
-        }
-    }, []);
+                if (storedHabilidades) {
+                        setHabilidades(JSON.parse(storedHabilidades));
+                }
 
-    return (
-        <Router>
-            <div>
-                <Navbar /> {/* Aquí agregamos el Navbar */}
-                <Routes>
-                    <Route path="/" element={<Inicio />} />
-                    <Route 
-                        path="/empleados" 
-                        element={<Empleados empleados={empleados} setEmpleados={setEmpleados} habilidades={habilidades} />} 
-                    />
-                    <Route 
-                        path="/habilidades" 
-                        element={<Habilidades habilidades={habilidades} setHabilidades={setHabilidades} empleados={empleados} />} 
-                    />
-                    <Route 
-                        path="/relacion" 
-                        element={<RelacionHabilidadesEmpleados empleados={empleados} habilidades={habilidades}/>} 
-                    />
-                    <Route path="/acerca-de" element={<AcercaDe />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-};
+                if (storedRelaciones) {
+                        setRelaciones(JSON.parse(storedRelaciones));
+                        
+                }
+        }, []);
 
-export default App;
+        return (
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li><Link to="/">Inicio</Link></li>
+                                <li><Link to="/empleados">Empleados</Link></li>
+                                <li><Link to="/habilidades">Habilidades</Link></li>
+                                <li><Link to="/relacion">Relaciones</Link></li>
+                                <li><Link to="/acerca-de">Acerca de</Link></li>
+                            </ul>
+                        </nav>
+        
+                        <Routes>
+                            <Route path="/" element={<Inicio />} />
+                            <Route
+                                path="/empleados"
+                                element={<Empleados empleados={empleados} setEmpleados={setEmpleados} habilidades={habilidades} />}
+                            />
+                            <Route
+                                path="/habilidades"
+                                element={<Habilidades habilidades={habilidades} setHabilidades={setHabilidades} empleados={empleados} />}
+                            />
+                            <Route
+                                path="/relacion"
+                                element={<RelacionHabilidadesEmpleados empleados={empleados} habilidades={habilidades} relaciones={relaciones} setRelaciones={setRelaciones} />}
+                            />
+                            <Route path="/acerca-de" element={<AcercaDe />} />
+                        </Routes>
+                    </div>
+                </Router>
+            );
+        };
+        
+        export default App;
